9811ce2c627fd50e3b798e6f929dc2e54c7fce1e
ali <hamzaali.tas@commencis.com>
2019-12-19 14:31:02 +0300

    chore(test-script): add test script for leaderboard

--- test.js
+++ test.js
@@ -1,42 +1,81 @@
-const Redis = require('ioredis');
+/**
+ * in order to run the test script with parameters use
+ * node test.js <app_base_url> <user_count> <submit_score_count> <step_count>
+ */
+const axios = require('axios');
 const faker = require('faker');
 
-const redis = new Redis(
-  'redis://h:p33eaa662908ffe23142361f8b75e94406fd00d4e45139a9f726b05de50bddfec@ec2-34-228-0-121.compute-1.amazonaws.com:27409',
-);
+const gjgLeaderboardClient = new axios.create({
+  baseURL: process.argv[2] || 'http://localhost:3000',
+});
 
-const testCount = 1000;
-async function generateMockData() {
-  const tasks = [];
-  for (let i = 0; i < testCount; i += 1) {
-    tasks.push(
-      redis.zadd(
-        `leaderboard:${faker.address.countryCode()}`,
-        faker.random.number(),
-        `${faker.name.firstName()}:${faker.random.uuid()}`,
-      ),
-    );
+/**
+ * main request to populate leaderboard and users
+ */
+async function createUser() {
+  try {
+    const response = await gjgLeaderboardClient({
+      method: 'post',
+      url: '/user/create',
+      data: {
+        display_name: `${faker.name.firstName()}${faker.random.number(100)}`,
+        country: faker.address.countryCode(),
+      },
+    });
+    const { user_id } = response.data;
+    return user_id;
+  } catch (error) {
+    console.error('CREATE USER ERROR', error);
   }
+}
 
-  await Promise.all(tasks);
+async function submitScore(userId) {
+  await gjgLeaderboardClient({
+    method: 'post',
+    url: '/score/submit',
+    data: {
+      user_id: userId,
+      score_worth: faker.random.number(1000000),
+    },
+  }).catch(error => {
+    console.error('SUBMIT SCORE ERROR', error);
+  });
+}
+
+async function bulkSteppedPromiseRunner(method, times, step) {
+  const results = [];
+  for (let i = 0; i < times; i += step) {
+    const result = await Promise.all([...Array(step)].map(() => method()));
+    results.push(...result);
+  }
+  return results;
+}
+
+const requestStep = process.argv[5] || 10;
+
+const userRequestCount = process.argv[3] || 100;
+async function createUserBulk() {
+  return bulkSteppedPromiseRunner(createUser, userRequestCount, requestStep);
+}
+
+const scoreSubmitRequestCount = process.argv[4] || 50;
+async function submitScores(userIds) {
+  bulkSteppedPromiseRunner(
+    () => {
+      const userIndex = faker.random.number(userIds.length - 1);
+      if (userIds[userIndex] == null) {
+        return;
+      }
+      submitScore(userIds[userIndex]);
+    },
+    scoreSubmitRequestCount,
+    requestStep,
+  );
 }
 
 async function main() {
-  //   const rank = await redis.zrevrank(
-  //     'leaderboard:HT',
-  //     'Marc:80d4e4b9-6bcb-4617-9d10-fb747dce9238',
-  //     // 'Amani:a63cdc12-90b3-4ba1-84d5-40572ede2153',
-  //   );
-  //   console.log('rank', rank);
-
-  const result = await redis.zrangebyscore(
-    'leaderboard:HT',
-    100,
-    90000,
-    'WITHSCORES',
-  );
-  console.log(result);
-  redis.disconnect();
+  const userIds = await createUserBulk();
+  await submitScores(userIds);
 }
 
 main().catch(console.error);
